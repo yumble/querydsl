997f5c4ab6487b82c2192aa7c2e807a03ab249d5
yumble <hnjn1101@gmail.com>
2023-02-06 00:16:58 +0900

    feat : 기본 쿼리함수 알기

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,34 @@
 package study.querydsl;
-
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+	@Autowired
+	EntityManager em;
 
-	@Test
-	void contextLoads() {
-	}
+	@Test void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
 
-}
+		Assertions.assertThat(result).isEqualTo(hello);
+		//lombok 동작 확인 (hello.getId())
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	} }
+
